8e260ac3e12ef89f5ba9a273790619796d573c90
Rohit Sahoo <rohitsahoo112003@gmail.com>
2025-11-25 14:42:20 +0530

    Service-registry

---
+++ service-registry/src/test/java/com/rohit/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
